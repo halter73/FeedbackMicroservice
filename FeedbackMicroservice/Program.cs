c3a1cbe7e46d6c1ec2e98ad761200b0822891791
Stephen Halter <halter73@gmail.com>
2021-10-25 19:04:38 -0700

    Submit feedback to Azure Table storage

--- FeedbackMicroservice/Program.cs
+++ FeedbackMicroservice/Program.cs
@@ -1,6 +1,37 @@
+using Azure.Data.Tables;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+var feedbackTable = new TableClient(app.Configuration.GetConnectionString("CosmosTableApi"), "Feedback");
+
+app.MapPost("/{appName}/feedback", async (string appName, Feedback feedback) =>
+{
+    if (feedback.Rating < 1 || feedback.Rating > 5)
+    {
+        return Results.BadRequest(new
+        {
+            Details = "Rating must be between 1 and 5!"
+        });
+    }
+
+    var entity = new TableEntity(appName, Guid.NewGuid().ToString())
+    {
+        ["Rating"] = feedback.Rating,
+        ["Comment"] = feedback.Comment,
+    };
+
+    await feedbackTable.AddEntityAsync(entity);
+
+    return Results.Created($"/{appName}/feedback/{entity.RowKey}", entity);
+});
+
+app.MapGet("/{appName}/feedback", (string appName) =>
+    feedbackTable.QueryAsync<TableEntity>($"PartitionKey eq '{appName}'"));
+
+app.MapGet("/{appName}/feedback/{id}", (string appName, Guid id) =>
+    feedbackTable.GetEntityAsync<TableEntity>(appName, id.ToString()));
 
 app.Run();
+
+record Feedback(int Rating, string? Comment);
